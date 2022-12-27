13a6624443740cb8301bb437b53a5b2abaaef5bf
ethicnology <ethicnology@pm.me>
2022-12-27 08:38:40 +0100

    chore: package template

---
+++ lib/nostr.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library nostr;
+
+export 'src/nostr_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
