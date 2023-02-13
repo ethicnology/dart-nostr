1c703205d46198f552e7a2428f70ec214aa8cf5a
ryzizub <dominik.simonik@gmail.com>
2023-02-13 14:10:14 +0100

    fix: pending bip340 issue

--- lib/src/keychain.dart
+++ lib/src/keychain.dart
@@ -10,38 +10,18 @@ class Keychain {
   late String public;
 
   /// Instantiate a Keychain with a private key hex-encoded
-  ///
-  /// There is a pending issue in dart-bip340 lib where some private keys generates non padded pubkey (missing leading '0').
-  /// If you input such private key, I can't let you Instantiates Keychain with the current private key because it will messup all NOSTR signatures.
-  /// I hope it will be fix asap: https://github.com/nbd-wtf/dart-bip340/issues/4
   Keychain(this.private) {
     assert(
       private.length == 64,
       "Private key should be 64 chars length (32 bytes hex encoded)",
     );
     public = bip340.getPublicKey(private);
-    assert(
-      public.length == 64,
-      '''\n
-      There is a pending issue in dart-bip340 lib where some private keys generates non padded pubkey (missing leading '0'). \n
-      I can't let you Instantiate Keychain with the current private key because it will messup all NOSTR signatures. \n
-      I hope it will be fix asap so i can remove this assert. \n
-      https://github.com/nbd-wtf/dart-bip340/issues/4
-      ''',
-    );
   }
 
   /// Instantiate a Keychain from random bytes
   Keychain.generate() {
     private = generate64RandomHexChars();
     public = bip340.getPublicKey(private);
-
-    /// The function getPublicKey() does not pad the returned value with 0 if the calculated public key should have zeros at beginning
-    /// https://github.com/nbd-wtf/dart-bip340/issues/4
-    while (public.length != 64) {
-      private = generate64RandomHexChars();
-      public = bip340.getPublicKey(private);
-    }
   }
 
   /// Encapsulate dart-bip340 sign() so you don't need to add bip340 as a dependency
