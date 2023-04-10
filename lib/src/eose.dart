a4d9392c26b07d96de9d3bc8f6143be95ceb1811
Nicolas Pulido <npulido@zonehacks.net>
2023-04-10 15:56:20 -0700

    clean up comments

--- lib/src/eose.dart
+++ lib/src/eose.dart
@@ -2,21 +2,21 @@ import 'dart:convert';
 
 import 'package:nostr/src/filter.dart';
 
-/// Used to request events and subscribe to new updates.
+/// Message sent by relay when reaching end of events
 class Eose {
   /// subscription_id is a random string that should be used to represent a subscription.
   late String subscriptionId;
 
   Eose(this.subscriptionId);
 
-  /// Serialize to nostr request message
-  /// - ["REQ", subscription_id, filter JSON, filter JSON, ...]
+  /// Serialize to nostr EOSE message
+  /// - ["EOSE", subscription_id]
   String serialize() {
     var header = jsonEncode(["EOSE", subscriptionId]);
     return '${header}';
   }
 
-  /// Deserialize a nostr eose message
+  /// Deserialize a nostr EOSE message
   /// - ["EOSE", subscription_id]
   Eose.deserialize(input) {
     assert(input.length == 2);
--- lib/src/ok.dart
+++ lib/src/ok.dart
@@ -3,22 +3,21 @@ import 'dart:convert';
 import 'package:nostr/src/filter.dart';
 
 class Ok {
-  late String subscriptionId;
+  late String string;
 
-  Ok(this.subscriptionId);
+  Ok(this.string);
 
-  /// Serialize to nostr request message
-  /// - ["REQ", subscription_id, filter JSON, filter JSON, ...]
+  /// Serialize to nostr OK message
+  /// - ["OK", string, bool, string]
   String serialize() {
-    var header = jsonEncode(["OK", subscriptionId]);
+    var header = jsonEncode(["OK", string]);
     return '${header}';
   }
 
   /// Deserialize a nostr OK message
-  /// - ["OK", subscription_id]
+  /// - ["OK", string]
   Ok.deserialize(input) {
-    assert(input.length == 2);
-    subscriptionId = input[1];
+    string = input[1];
   }
 
   bool isValid() {
