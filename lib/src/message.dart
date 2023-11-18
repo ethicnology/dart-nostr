8995114c59146e7b19fcaf61ed85bc4102d37d53
Uchijo <kgtnttki.yuta@gmail.com>
2023-11-18 18:39:03 +0900

    added MessageType enum and concreteType to Message

--- lib/src/message.dart
+++ lib/src/message.dart
@@ -5,35 +5,53 @@ import 'package:nostr/nostr.dart';
 // Used to deserialize any kind of message that a nostr client or relay can transmit.
 class Message {
   late String type;
+  late MessageType concreteType;
   late dynamic message;
 
 // nostr message deserializer
   Message.deserialize(String payload) {
     dynamic data = jsonDecode(payload);
-    var messages = ["EVENT", "REQ", "CLOSE", "NOTICE", "EOSE", "OK", "AUTH"];
-    if (messages.contains(data[0]) == false) {
+    if (MessageType.values.map<String>((e) => e.rawType).contains(data[0]) ==
+        false) {
       throw 'Unsupported payload (or NIP)';
     }
 
     type = data[0];
-    switch (type) {
-      case "EVENT":
+    concreteType = MessageType.byRawType(data[0]);
+    switch (concreteType) {
+      case MessageType.event:
         message = Event.deserialize(data);
         // ignore: deprecated_member_use_from_same_package
         if (message.kind == 4) message = EncryptedDirectMessage(message);
         break;
-      case "OK":
+      case MessageType.ok:
         message = Nip20.deserialize(data);
         break;
-      case "REQ":
+      case MessageType.req:
         message = Request.deserialize(data);
         break;
-      case "CLOSE":
+      case MessageType.close:
         message = Close.deserialize(data);
         break;
       default:
         message = jsonEncode(data.sublist(1));
         break;
     }
+  }
+}
+
+enum MessageType {
+  event("EVENT"),
+  req("REQ"),
+  close("CLOSE"),
+  notice("NOTICE"),
+  eose("EOSE"),
+  ok("OK"),
+  auth("AUTH");
+
+  const MessageType(this.rawType);
+  final String rawType;
+  static MessageType byRawType(String name) {
+    return MessageType.values.byName(name.toLowerCase());
   }
 }
