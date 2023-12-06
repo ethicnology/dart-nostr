1929df78000a72dbf30a1f5adc5cdc4720af959c
Uchijo <kgtnttki.yuta@gmail.com>
2023-12-06 23:43:30 +0900

    Message.deserialize can handle EOSE

--- lib/src/eose.dart
+++ lib/src/eose.dart
@@ -19,7 +19,7 @@ class Eose {
   /// Deserialize a nostr close message
   /// - ["CLOSE", subscription_id]
   factory Eose.deserialize(input) {
-    if (input is! List<String>) throw 'Invalid type for EOSE message';
+    if (input is! List<dynamic>) throw 'Invalid type for EOSE message';
     if (input.length != 2) throw 'Invalid length for EOSE message';
     return Eose(input[1]);
   }
