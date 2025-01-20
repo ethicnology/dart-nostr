2c1d6ff9ca0096843f59a1920db346b912a3a976
ethicnology <ethicnology@pm.me>
2025-01-20 09:06:29 -0500

    test: NIP9

---
+++ test/nips/nip_009_test.dart
@@ -0,0 +1,61 @@
+import 'package:nostr/nostr.dart';
+import 'package:test/test.dart';
+
+const privkey =
+    '5ee1c8000ab28edd64d74a7d951ac2dd559814887b1b9e1ac7c5f89e96125c12';
+const pubkey =
+    '0ba0206887bd61579bf65ec09d7806bea32c64be1cf2c978cf031a811cd238db';
+
+/// Unit Tests for Nip9
+void main() {
+  test('toTags should convert event IDs to tags', () {
+    List<String> eventIds = ["event1", "event2"];
+    List<List<String>> expectedTags = [
+      ["e", "event1"],
+      ["e", "event2"]
+    ];
+    expect(Nip9.toTags(eventIds), equals(expectedTags));
+  });
+
+  test('tagsToList should extract event IDs from tags', () {
+    List<List<String>> tags = [
+      ["e", "event1"],
+      ["e", "event2"]
+    ];
+    List<String> expectedEventIds = ["event1", "event2"];
+    expect(Nip9.tagsToList(tags), equals(expectedEventIds));
+  });
+
+  test('encode should create a valid Event object', () {
+    List<String> eventIds = ["event1", "event2"];
+    String content = "Reason";
+
+    Event event = Nip9.encode(eventIds, content, pubkey, privkey);
+    expect(event.kind, equals(5));
+    expect(
+        event.tags,
+        equals([
+          ["e", "event1"],
+          ["e", "event2"]
+        ]));
+    expect(event.content, equals(content));
+    expect(event.pubkey, equals(pubkey));
+  });
+
+  test('decode should convert a valid Event to a DeleteEvent', () {
+    Event event = Event.from(
+      kind: 5,
+      tags: [
+        ["e", "event1"],
+        ["e", "event2"]
+      ],
+      content: "Reason",
+      pubkey: pubkey,
+      privkey: privkey,
+    );
+    DeleteEvent deleteEvent = Nip9.decode(event);
+    expect(deleteEvent.pubkey, equals(pubkey));
+    expect(deleteEvent.deleteEvents, equals(["event1", "event2"]));
+    expect(deleteEvent.reason, equals("Reason"));
+  });
+}
